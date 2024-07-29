be7223d7f8dfb3bcac0beb76d157b6727c9657ae
SamuelEscalera <samuel.escalera.9413603@usalesiana.edu.bo>
2024-07-29 11:29:54 -0400

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:counter_flutter/presentation/screens/counter/counter_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: CounterScreen());
+  }
+}
